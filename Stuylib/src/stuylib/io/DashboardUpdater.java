eb7daf28918b9b28546e410550baa8ade1d96acb
Kevin Wang <born2run5000@yahoo.com>
2011-07-19 22:56:21 +0800

    Created DashboardUpdater class. Did not use the name "Dashboard" because it would conflict with the WPILibJ class with the same name.

---
+++ Stuylib/src/stuylib/io/DashboardUpdater.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stuylib.io;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * Handles the transmission of user-defined data to a PC running LabVIEW dashboard software.
+ * 
+ * @author Kevin Wang
+ */
+public class DashboardUpdater {
+    
+    /**
+     * Starts a timer to update the dashboard every 0.1 seconds.
+     */
+    public DashboardUpdater() {
+        Timer dashTimer = new Timer();
+        dashTimer.schedule(new TimerTask() {
+            public void run() {
+                updateDashboard();
+            }
+        }, 0, 100);
+    }
+    
+    /**
+     * Packs and sends data to be displayed on the dashboard.
+     * 
+     * Note that any changes to the dashboard packet structure made here must
+     * also be reflected in the dashboard's LabVIEW code.
+     */
+    public void updateDashboard() {
+        //Dashboard lowDashData = DriverStation.getInstance().getDashboardPackerLow();
+        
+        // Pack stuff
+        
+        //lowDashData.commit();
+    }
+}
