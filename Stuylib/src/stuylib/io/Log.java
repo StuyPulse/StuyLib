5dc8c703f8f6c5764b195ecb7628c6638ce08b83
Alejandro Carrillo <aecarrillo594@gmail.com>
2011-07-21 00:19:46 -0400

    established the logging protocol: [<timestamp> @<calling_object>] <Message>

--- Stuylib/src/stuylib/io/Log.java
+++ Stuylib/src/stuylib/io/Log.java
@@ -5,21 +5,23 @@
 package stuylib.io;
 
 //TODO: Import WPIlibj stuff
+
 /**
  *
  * @author Alejandro Carrillo
  */
 public class Log {
-    
+
     private StringBuffer concatString;
     private int logState;
+    private static final int DEBUG_STDOUT_MODE = 0;
+    private static final int DEBUG_CATSTR_MODE = 1;
+    private static final int FMS_WRITELOG_MODE = 2;
 
-    private static final int DEBUG_STDOUT_MODE  = 0;
-    private static final int DEBUG_CATSTR_MODE  = 1;
-    private static final int FMS_WRITELOG_MODE  = 2;
-
+    /**
+     * @param state
     public Log(int state) {
-       logState = state;
+    logState = state;
     }
     /**
      * Determines what to do with input:
@@ -28,26 +30,26 @@ public class Log {
      * 3. Append data to StringBuffer, then print StringBuffer to stdout when disabled
      * 
      * Uses both user specifications and FMS data to determine which route it can take
-     * @param s 
+     * @param s String to be logged
+     * @param writer calling object
      */
-    public void write(String s) {
+    public void write(String s, Object writer) {
         /* pseudocode */
-        
-         switch (logState) {
-        //TODO:  Implement this
-         
-         case DEBUG_STDOUT_MODE:
-             System.out.println(s);
-             break;
-         case DEBUG_CATSTR_MODE:
-             concatString.append(s);
-             break;
-         case FMS_WRITELOG_MODE:
-             //Write to a file
-             break;
+        String prefix = "[" + System.currentTimeMillis() + " @" + writer.getClass().getName() + " ] "; // Prints out timestamp and origin
+        switch (logState) {
 
-         
-         }
-    
+            case DEBUG_STDOUT_MODE:
+                System.out.println(prefix + s);
+                break;
+            case DEBUG_CATSTR_MODE:
+                concatString.append(prefix + s + "\n");
+                break;
+            case FMS_WRITELOG_MODE:
+                //Write to a file
+                break;
+
+
+        }
+
     }
 }
