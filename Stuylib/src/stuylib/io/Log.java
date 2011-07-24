5b82f3dee10c35e4e47c8a94277ae027487cde39
Alejandro Carrillo <aecarrillo594@gmail.com>
2011-07-23 23:22:46 -0400

    Configured library to allow WPIlibj and com.squawk.microedition imports, finished implementing the Log class, closes #2

--- Stuylib/src/stuylib/io/Log.java
+++ Stuylib/src/stuylib/io/Log.java
@@ -4,7 +4,10 @@
  */
 package stuylib.io;
 
-//TODO: Import WPIlibj stuff
+import com.sun.squawk.microedition.io.FileConnection;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import javax.microedition.io.Connector;
 
 /**
  *
@@ -12,44 +15,116 @@ package stuylib.io;
  */
 public class Log {
 
-    private StringBuffer concatString;
+    private StringBuffer logString; // For DEBUG_CATSTR mode
     private int logState;
-    private static final int DEBUG_STDOUT_MODE = 0;
-    private static final int DEBUG_CATSTR_MODE = 1;
-    private static final int FMS_WRITELOG_MODE = 2;
+    // Three possible states
+    private static final int DEBUG_STDOUT = 0;  // For printing log data to stdout, the default mode
+    private static final int DEBUG_CATSTR = 1;  // For appending log data to a string, which is printed at the end
+    private static final int FMS_WRITELOG = 2;  // When in FMS mode, will write data to a log file
+    private OutputStreamWriter writer;
 
     /**
-     * @param state
-    public Log(int state) {
-    logState = state;
-    }
-    /**
-     * Determines what to do with input:
-     * 1. Print to stdout
-     * 2. Write to log file
-     * 3. Append data to StringBuffer, then print StringBuffer to stdout when disabled
-     * 
-     * Uses both user specifications and FMS data to determine which route it can take
-     * @param s String to be logged
-     * @param writer calling object
+     * If DEBUG_STDOUT is passed, then the write() method will print data to stdout
+     * If DEBUG_CATSTR is passed, then the write() method will append data to a String, which is printed once
+     * Finally, passing FMS_WRITELOG will write data to a logfile
+     *
+     * @param state Either DEBUG_STDOUT, DEBUG_CATSTR, or FMS_WRITELOG.  Using any other value will default to DEBUG_STDOUT behavior.
      */
-    public void write(String s, Object writer) {
+    public Log(int state) {
+        logState = state;
+        if (logState == DEBUG_CATSTR) {
+            logString = new StringBuffer("");
+        }
+        else if (logState == FMS_WRITELOG) {
+            try {
+                String url = "file:///competition.log";
+                FileConnection c = (FileConnection) Connector.open(url);
+                writer = new OutputStreamWriter(c.openOutputStream());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Prepares regular input for writing via the writer() method
+     * @param s message string
+     * @param logger calling object
+     */
+    public void write(String message, Object logger) {
         /* pseudocode */
-        String prefix = "[" + System.currentTimeMillis() + " @" + writer.getClass().getName() + " ] "; // Prints out timestamp and origin
+        String prefix = "[" + System.currentTimeMillis() + " @" + logger.getClass().getName() + " ] INFO - "; // Records timestamp and origin
+        String data = prefix + message;
+
+        writer(data);  // Logs the message string
+    }
+
+    /**
+     * Prepares exception data for writing via the writer() method
+     *
+     * @param e Exception object
+     * @param logger calling object
+     */
+    public void write(Exception e, Object logger) {
+        /* pseudocode */
+        String prefix = "[" + System.currentTimeMillis() + " @" + logger.getClass().getName() + " ] ERROR - "; // Records timestamp and origin
+        String data = prefix + e.getMessage(); // Creates data String from Exception data
+
+        writer(data); // Logs the message String
+    }
+
+    /**
+     * Outputs data to one of three places:
+     * 1. stdout if we're in DEBUG_STDOUT mode
+     * 2. the end of a string, which will be printed out later
+     * 3. a logfile if we're in FMS_WRITELOG mode
+     *
+     * @param data the data to be written
+     */
+    private void writer(String data) {
         switch (logState) {
 
-            case DEBUG_STDOUT_MODE:
-                System.out.println(prefix + s);
+            case DEBUG_CATSTR:
+                logString.append(data + "\n"); // Appends data to the logString
                 break;
-            case DEBUG_CATSTR_MODE:
-                concatString.append(prefix + s + "\n");
+            case FMS_WRITELOG:
+                try {
+                    writer.write(data);        // Saves data to a logfile on the cRIO
+                } catch (IOException f) {
+                    f.printStackTrace();
+                }
                 break;
-            case FMS_WRITELOG_MODE:
-                //Write to a file
+            default:
+                System.out.println(data);      // Prints data to stdout, the default behavior
                 break;
+        }
+    }
 
+    /**
+     * MUST be called in order to close logfile or print data.
+     *
+     * It is recommended that this be called once when trasitioning from enabled -> disabled,
+     * e.g. right after the program exits the operatorControl() method's while loop if you use SimpleRobot.
+     *
+     * This method should be called regardless of mode.  If in DEBUG_STDOUT, this method does nothing, so it's safe.
+     */
+    public void close() {
+        if (logState == FMS_WRITELOG) {
+            try {
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
+        } else if (logState == DEBUG_CATSTR) {
+            System.out.println(logString); // Print out all the data collected over time via DEBUG_CATSTR mode
         }
 
     }
 }
+
+
+
+
+
+
