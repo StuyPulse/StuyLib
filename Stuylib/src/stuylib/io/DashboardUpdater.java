cd905ea54124e595ba18e17dc72bb9cb1f70e93c
Kevin Wang <born2run5000@yahoo.com>
2011-08-06 01:48:07 +0800

    Added lots of documentation for DashboardUpdater.

--- Stuylib/src/stuylib/io/DashboardUpdater.java
+++ Stuylib/src/stuylib/io/DashboardUpdater.java
@@ -11,30 +11,38 @@ import java.util.TimerTask;
 
 /**
  * Handles the transmission of user-defined data to a PC running LabVIEW dashboard software.
+ * Important: All instances of "Object" in this class must be replaced by the name of the main robot class in order to access sensor data.
  * 
  * @author Kevin Wang
  */
 public class DashboardUpdater {
+    // Quantities of each variable type
     private final int NUM_BOOLEANS = 20;
     private final int NUM_DOUBLES = 20;
     private final int NUM_INTS = 5;
     private final int NUM_STRINGS = 5;
     
+    // Period at which to update the dashboard
     private final int UPDATE_PERIOD_MS = 100;
     
+    // Buffer to store data to be sent to the dashboard
     private boolean[] booleans = new boolean[NUM_BOOLEANS];
     private double[] doubles = new double[NUM_DOUBLES];
     private int[] ints = new int[NUM_INTS];
     private String[] strings = new String[NUM_STRINGS];
     
+    // Buffer indices for auto-incrementation
     private int booleansIndex, doublesIndex, intsIndex, stringsIndex;
     
-    private Object robot;
+    // Reference to the main robot object
+    private Object robot; // "Object" MUST be replaced by the main robot class
     
     /**
-     * Starts a timer to update the dashboard every 0.1 seconds.
+     * Initializes the buffer and starts a timer to update the dashboard every 0.1 seconds.
+     * 
+     * @param robot Reference to the main robot object
      */
-    public DashboardUpdater(Object robot) {
+    public DashboardUpdater(Object robot) { // "Object" MUST be replaced by the main robot class
         this.robot = robot;
         resetBuffer();
         
@@ -42,23 +50,25 @@ public class DashboardUpdater {
         dashTimer.schedule(new TimerTask() {
             @Override
             public void run() {
-                updateDashboard();
+                defineDashboardDatatype();
+                commit();
             }
         }, 0, UPDATE_PERIOD_MS);
     }
     
-    private void updateDashboard() {
+    /**
+     * Writes data to the buffer to be committed to the dashboard using commit().
+     * This is where changes should be made to specify what data should be sent to the dashboard.
+     */
+    private void defineDashboardDatatype() {
 //        addBoolean(true);
 //        addDouble(6.94);
 //        addInt(694);
 //        addString("Stuypulse");
-        
-        commit();
     }
     
     /**
-     * Packs and sends data to be displayed on the dashboard.
-     * 
+     * Commits the contents of the buffer to the dashboard, and then resets the buffer.
      * Note that any changes to the dashboard packet structure made here must
      * also be reflected in the dashboard's LabVIEW code.
      */
@@ -137,6 +147,9 @@ public class DashboardUpdater {
         strings[index] = value;
     }
     
+    /**
+     * Clears the buffer and resets all indices to zero.
+     */
     private void resetBuffer() {
         for (int i = 0; i < booleans.length; i++)
             booleans[i] = false;
