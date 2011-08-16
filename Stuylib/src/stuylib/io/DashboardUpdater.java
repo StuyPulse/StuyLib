ea88a51e3d574a7d7a9fc31b390134d81ebd02ab
Eric Lam <captainpicardlam@gmail.com>
2011-08-16 00:18:23 -0400

    Added a cluster for Enhanced I/O stuff. Put in spaces for four joysticks. Am unsure if it is completely correct, but am fairly sure.

--- Stuylib/src/stuylib/io/DashboardUpdater.java
+++ Stuylib/src/stuylib/io/DashboardUpdater.java
@@ -24,6 +24,17 @@ public class DashboardUpdater {
     private final int DIGITAL_MODULE_1 = 4;
     private final int DIGITAL_MODULE_2 = 6;
   
+    // Enhanced I/O Stuff. Joystick values will probably change?
+    private final int LEFT_STICK_PORT = 1;
+    private final int RIGHT_STICK_PORT = 2;
+    private final int EXTRA_STICK_1_PORT = 3;
+    private final int EXTRA_STICK_2_PORT = 4;
+    // Amount of buttons on each joystick. 11 is using the assumption of Attack 3's.
+    private final int LEFT_STICK_BUTTONS = 11;
+    private final int RIGHT_STICK_BUTTONS = 11;
+    private final int EXTRA_STICK_1_BUTTONS = 11;
+    private final int EXTRA_STICK_2_BUTTONS = 11;
+    
     // Quantities of each variable type
     private final int NUM_BOOLEANS = 20;
     private final int NUM_DOUBLES = 20;
@@ -89,6 +100,7 @@ public class DashboardUpdater {
             // Analog Modules
             lowDashData.addCluster();
             {
+                // Iterates through Analogue Ports (on the first Analog Module) and sends data from each to the Dashboard.
                 lowDashData.addCluster();
                 {
                     for (int i = 1; i <= 8; i++) {
@@ -96,6 +108,7 @@ public class DashboardUpdater {
                     }
                 }
                 lowDashData.finalizeCluster();
+                // Iterates through Analoge Ports (on the second Analog Module) and sends data from each to the Dashboard.
                 lowDashData.addCluster();
                 {
                     for (int i = 1; i <= 8; i++) {
@@ -110,14 +123,14 @@ public class DashboardUpdater {
             lowDashData.addCluster();
             {  // Digital Module 1
                 lowDashData.addCluster();
-                {
+                {   // Sends relays and Digital I/O to Dashboard.
                     int module = DIGITAL_MODULE_1;
                     lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
                     lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
                     lowDashData.addShort(DigitalModule.getInstance(module).getAllDIO());
                     lowDashData.addShort(DigitalModule.getInstance(module).getDIODirection());
                     lowDashData.addCluster();
-                    {
+                    {   // Iterates through PWM ports and sends data to dashboard.
                         for (int i = 1; i <= 10; i++) {
                         lowDashData.addByte((byte) DigitalModule.getInstance(module).getPWM(i));
                     }
@@ -128,14 +141,14 @@ public class DashboardUpdater {
 
                 // Digital Module 2
                 lowDashData.addCluster();
-                {
+                {   // Sends relays and Digital I/O to Dashboard.
                     int module = DIGITAL_MODULE_2;
                     lowDashData.addByte(DigitalModule.getInstance(module).getRelayForward());
                     lowDashData.addByte(DigitalModule.getInstance(module).getRelayReverse());
                     lowDashData.addShort(DigitalModule.getInstance(module).getAllDIO());
                     lowDashData.addShort(DigitalModule.getInstance(module).getDIODirection());
                     lowDashData.addCluster();
-                    {
+                    {   // Iterates through PWM ports and sends data to dashboard.
                         for (int i = 1; i <= 10; i++) {
                         lowDashData.addByte((byte) DigitalModule.getInstance(module).getPWM(i));
                     }
@@ -147,9 +160,103 @@ public class DashboardUpdater {
             }
             lowDashData.finalizeCluster();
 
+            // Not really sure what this byte is for, but it was there with DESDroid...
             lowDashData.addByte((byte) 0);
         }
         lowDashData.finalizeCluster();
+        
+        
+        // Enhanced I/O data
+        lowDashData.addCluster();
+        {
+          // Joysticks
+          lowDashData.addCluster();
+          { // Left Joystick
+            lowDashData.addCluster();
+            { // Joystick X and Y Axes
+              lowDashData.addCluster();
+              {
+                lowDashData.addDouble(Joystick(LEFT_STICK_PORT).getX());
+                lowDashData.addDouble(Joystick(LEFT_STICK_PORT).getY());
+              }
+              lowDashData.finalizeCluster();
+              // Adds Joystick Buttons
+              lowDashData.addCluster();
+              {
+                for (int i = 1; i <= LEFT_STICK_BUTTONS; i++) {
+                        lowDashData.addBoolean(Joystick(LEFT_STICK_PORT).getRawButton(i));
+                    }
+              }
+              lowDashData.finalizeCluster();
+            }
+            lowDastData.finalizeCluster();
+            
+            // Right Joystick
+            lowDashData.addCluster();
+            { // Joystick X and Y Axes
+              lowDashData.addCluster();
+              {
+                lowDashData.addDouble(Joystick(RIGHT_STICK_PORT).getX());
+                lowDashData.addDouble(Joystick(RIGHT_STICK_PORT).getY());
+              }
+              lowDashData.finalizeCluster();
+              // Adds Joystick Buttons
+              lowDashData.addCluster();
+              {
+                for (int i = 1; i <= RIGHT_STICK_BUTTONS; i++) {
+                        lowDashData.addBoolean(Joystick(RIGHT_STICK_PORT).getRawButton(i));
+                    }
+              }
+              lowDashData.finalizeCluster();
+            }
+            lowDastData.finalizeCluster();
+            
+            // Extra Joystick 1
+            lowDashData.addCluster();
+            { // Joystick X and Y Axes
+              lowDashData.addCluster();
+              {
+                lowDashData.addDouble(Joystick(EXTRA_STICK_1_PORT).getX());
+                lowDashData.addDouble(Joystick(EXTRA_STICK_1_PORT).getY());
+              }
+              lowDashData.finalizeCluster();
+              // Adds Joystick Buttons
+              lowDashData.addCluster();
+              {
+                for (int i = 1; i <= EXTRA_STICK_1_BUTTONS; i++) {
+                        lowDashData.addBoolean(Joystick(EXTRA_STICK_1_BUTTONS).getRawButton(i));
+                    }
+              }
+              lowDashData.finalizeCluster();
+            }
+            lowDastData.finalizeCluster();
+            
+            // Extra Joystick 2
+            lowDashData.addCluster();
+            { // Joystick X and Y Axes
+              lowDashData.addCluster();
+              {
+                lowDashData.addDouble(Joystick(EXTRA_STICK_2_PORT).getX());
+                lowDashData.addDouble(Joystick(EXTRA_STICK_2_PORT).getY());
+              }
+              lowDashData.finalizeCluster();
+              // Adds Joystick Buttons
+              lowDashData.addCluster();
+              {
+                for (int i = 1; i <= EXTRA_STICK_2_BUTTONS; i++) {
+                        lowDashData.addBoolean(Joystick(EXTRA_STICK_2_BUTTONS).getRawButton(i));
+                    }
+              }
+              lowDashData.finalizeCluster();
+            }
+            lowDastData.finalizeCluster();
+          }
+          lowDashData.finalizeCluster();
+          
+          // Other Enhanced I/O (put here)
+        }
+        lowDashData.finalizeCluster();
+        
         
         // Non-sensor data
         lowDashData.addCluster();
