18076718d29df9309a229a8a6a11a3fd38e65f39
Q <zqiu1994@gmail.com>
2011-08-23 20:37:49 -0400

    Added gyro

--- Stuylib/src/stuylib/drive/prototype/DriveTrain.java
+++ Stuylib/src/stuylib/drive/prototype/DriveTrain.java
@@ -9,6 +9,8 @@ public class DriveTrain {
     VictorSpeed rear_left_motor;
     VictorSpeed rear_right_motor;
 
+    Gyro g;
+
     DriveTrain(DriveConfig O) {
         front_left_motor = new VictorSpeed(O.getFrontLeftMotorChannel(),
                 O.getFrontLeftEncoderChannel()[0], O.getFrontLeftEncoderChannel()[1], false, O);
@@ -18,5 +20,7 @@ public class DriveTrain {
                 O.getRearLeftEncoderChannel()[0], O.getRearLeftEncoderChannel()[1], false, O);
         rear_right_motor = new VictorSpeed(O.getRearRightMotorChannel(),
                 O.getRearRightEncoderChannel()[0], O.getRearRightEncoderChannel()[1], true, O);
+
+        g = new Gyro(O.getGyroChannel());
     }
 }
