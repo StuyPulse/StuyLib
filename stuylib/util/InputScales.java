e9618aea46b3f45a0b182bc7bc41aa179fcf2c0e
Sam Belliveau <sam.belliveau@gmail.com>
2019-09-27 11:06:11 -0400

    added input scales

---
+++ stuylib/util/InputScales.java
@@ -0,0 +1,44 @@
+package stuylib.util;
+
+public class InputScales {
+    
+    /**** What Is The Input Scalar Class? ****
+     * The input scalar class is a class
+     * that packages many different 
+     * functions that can help with 
+     * squaring inputs etc.
+     */
+
+    public static double limit(double x) {
+        return Math.max(Math.min(x, 1.0), -1.0);
+    }
+
+    public static double square(double x) {
+        return limit(x * x * Math.signum(x));
+    }
+
+    public static double cube(double x) {
+        return limit(x * x * x);
+    }
+
+    public static double pow(double x, double power) {
+        return limit(Math.pow(Math.abs(x), power) * Math.signum(x));
+    }
+
+    /**
+     * As apposed to squaring and cubing inputs
+     * to get a finer control over smaller values,
+     * I prepose this circular algorithm. 
+     * https://www.desmos.com/calculator/msqknwjdk7
+     * /\ Shows how it gives a nicer, more rounded
+     * curve which still speeds up, but also gives
+     * the ability for fine control
+     */
+    public static double circular(double x) {
+        double sign = Math.signum(x);
+        x = Math.sqrt(1.0 - square(x));
+        x = 1.0 - x;
+        x *= sign;
+        return limit(x);
+    }
+}
