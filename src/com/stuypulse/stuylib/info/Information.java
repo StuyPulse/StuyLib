e64a3b628369a780d1bf0fa24e8c78a97a5ad437
Sam Belliveau <sam.belliveau@gmail.com>
2019-11-13 16:46:13 -0500

    changed version

--- src/com/stuypulse/stuylib/info/Information.java
+++ src/com/stuypulse/stuylib/info/Information.java
@@ -7,7 +7,7 @@ import com.stuypulse.stuylib.info.Version;
  */
 public interface Information {
     // Current version of stuylib
-    final Version STUYLIB_VER = new Version(1,5);
+    final Version STUYLIB_VER = new Version(1,5,1);
 
     // Name of library + Version
     final String LIB_NAME = "StuyLib " + STUYLIB_VER;
