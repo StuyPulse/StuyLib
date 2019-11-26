3fa9acf0005c7583aee18310e3407c4a2821fe87
Sam Belliveau <sam.belliveau@gmail.com>
2019-11-26 12:57:39 -0500

    simplified gamepad class

--- src/com/stuypulse/stuylib/input/gamepads/Logitech.java
+++ src/com/stuypulse/stuylib/input/gamepads/Logitech.java
@@ -12,35 +12,16 @@ import edu.wpi.first.wpilibj.GenericHID;
 
 public class Logitech extends WPIGamepad {
 
-    /********************/
-    /*** CONSTRUCTORS ***/
-    /********************/
+    // Constructor //
     public Logitech(int port) {
         super(port);
     }
-
-    /**********************/
-    /*** D-PAD CONTROLS ***/
-    /**********************/
-    @Override
-    public boolean getRawDPadUp() {
-        return getJoystick().getPOV() == 0;
-    }
-
-    @Override
-    public boolean getRawDPadDown() {
-        return getJoystick().getPOV() == 180;
-    }
-
-    @Override
-    public boolean getRawDPadLeft() {
-        return getJoystick().getPOV() == 270;
-    }
-
-    @Override
-    public boolean getRawDPadRight() {
-        return getJoystick().getPOV() == 90;
-    }
+    
+    // D-Pad //
+    public boolean getRawDPadUp()       { return getJoystick().getPOV() == 0; }
+    public boolean getRawDPadDown()     { return getJoystick().getPOV() == 180; }
+    public boolean getRawDPadLeft()     { return getJoystick().getPOV() == 270; }
+    public boolean getRawDPadRight()    { return getJoystick().getPOV() == 90; }
 
     /**
      * There is a switch on the back of the controller this is for when the switch
@@ -48,136 +29,43 @@ public class Logitech extends WPIGamepad {
      */
     public static class DMode extends Logitech {
 
-        /********************/
-        /*** CONSTRUCTORS ***/
-        /********************/
+        // Constructor //
         public DMode(int port) {
             super(port);
         }
 
-        /**********************/
-        /*** CONTROL STICKS ***/
-        /**********************/
-        @Override
-        public double getLeftX() {
-            return getRawAxis(0);
-        }
+        // Left Stick //
+        public double getLeftX()    { return getRawAxis(0); }
+        public double getLeftY()    { return -getRawAxis(1); }
 
-        @Override
-        public double getLeftY() {
-            return -getRawAxis(1);
-        }
+        // Right Stick //
+        public double getRightX()   { return getRawAxis(4); }
+        public double getRightY()   { return -getRawAxis(3); }
 
-        @Override
-        public double getRightX() {
-            return getRawAxis(4);
-        }
+        // Bumpers //
+        public boolean getRawLeftBumper()   { return getRawButton(5); }
+        public boolean getRawRightBumper()  { return getRawButton(6); }
 
-        @Override
-        public double getRightY() {
-            return -getRawAxis(3);
-        }
+        // Triggers //
+        public double getRawLeftTriggerAxis()   { return getRawButton(7) ? 0.0 : 1.0; }
+        public double getRawRightTriggerAxis()  { return getRawButton(8) ? 0.0 : 1.0; }
 
-        /*******************/
-        /*** LEFT BUMPER ***/
-        /*******************/
-        @Override
-        public boolean getRawLeftBumper() {
-            return getRawButton(5);
-        }
+        // Face Buttons //
+        public boolean getRawLeftButton()   { return getRawButton(1); }
+        public boolean getRawRightButton()  { return getRawButton(3); }
+        public boolean getRawTopButton()    { return getRawButton(4); }
+        public boolean getRawBottomButton() { return getRawButton(2); }
 
-        /********************/
-        /*** RIGHT BUMPER ***/
-        /********************/
-        @Override
-        public boolean getRawRightBumper() {
-            return getRawButton(6);
-        }
+        // Start / Select / Option //
+        public boolean getRawSelectButton() { return getRawButton(9); }
+        public boolean getRawStartButton()  { return getRawButton(10); }
+        public boolean getRawOptionButton() { return getRawButton(10); }
 
-        /********************/
-        /*** LEFT TRIGGER ***/
-        /********************/
-        @Override
-        public double getRawLeftTriggerAxis() {
-            return getRawButton(7) ? 0.0 : 1.0;
-        }
+        // Analog Stick Buttons // 
+        public boolean getRawLeftAnalogButton()     { return getRawButton(11); }
+        public boolean getRawRightAnalogButton()    { return getRawButton(12); }
 
-        /*********************/
-        /*** RIGHT TRIGGER ***/
-        /*********************/
-        @Override
-        public double getRawRightTriggerAxis() {
-            return getRawButton(8) ? 0.0 : 1.0;
-        }
-
-        /*******************/
-        /*** LEFT BUTTON ***/
-        /*******************/
-        @Override
-        public boolean getRawLeftButton() {
-            return getRawButton(1);
-        }
-
-        /********************/
-        /*** RIGHT BUTTON ***/
-        /********************/
-        @Override
-        public boolean getRawRightButton() {
-            return getRawButton(3);
-        }
-
-        /******************/
-        /*** TOP BUTTON ***/
-        /******************/
-        @Override
-        public boolean getRawTopButton() {
-            return getRawButton(4);
-        }
-
-        /*********************/
-        /*** BOTTOM BUTTON ***/
-        /*********************/
-        @Override
-        public boolean getRawBottomButton() {
-            return getRawButton(2);
-        }
-
-        /*********************/
-        /*** SELECT BUTTON ***/
-        /*********************/
-        @Override
-        public boolean getRawSelectButton() {
-            return getRawButton(9);
-        }
-
-        /********************/
-        /*** START BUTTON ***/
-        /********************/
-        @Override
-        public boolean getRawStartButton() {
-            return getRawButton(10);
-        }
-
-        /**************************/
-        /*** LEFT ANALOG BUTTON ***/
-        /**************************/
-        @Override
-        public boolean getRawLeftAnalogButton() {
-            return getRawButton(11);
-        }
-
-        /***************************/
-        /*** RIGHT ANALOG BUTTON ***/
-        /***************************/
-        @Override
-        public boolean getRawRightAnalogButton() {
-            return getRawButton(12);
-        }
-
-        /**************/
-        /*** RUMBLE ***/
-        /**************/
-        @Override
+        // Rumble //
         public void setRumble(double intensity) {
             getJoystick().setRumble(GenericHID.RumbleType.kLeftRumble, intensity);
             getJoystick().setRumble(GenericHID.RumbleType.kRightRumble, intensity);
@@ -190,136 +78,43 @@ public class Logitech extends WPIGamepad {
      */
     public static class XMode extends Logitech {
 
-        /********************/
-        /*** CONSTRUCTORS ***/
-        /********************/
+        // Constructor //
         public XMode(int port) {
             super(port);
         }
 
-        /**********************/
-        /*** CONTROL STICKS ***/
-        /**********************/
-        @Override
-        public double getLeftX() {
-            return getRawAxis(0);
-        }
+        // Left Stick //
+        public double getLeftX()    { return getRawAxis(0); }
+        public double getLeftY()    { return -getRawAxis(1); }
 
-        @Override
-        public double getLeftY() {
-            return -getRawAxis(1);
-        }
+        // Right Stick //
+        public double getRightX()   { return getRawAxis(4); }
+        public double getRightY()   { return -getRawAxis(3); }
 
-        @Override
-        public double getRightX() {
-            return getRawAxis(4);
-        }
+        // Bumpers //
+        public boolean getRawLeftBumper()   { return getRawButton(5); }
+        public boolean getRawRightBumper()  { return getRawButton(6); }
 
-        @Override
-        public double getRightY() {
-            return -getRawAxis(5);
-        }
+        // Triggers //
+        public double getRawLeftTriggerAxis()   { return getRawAxis(2); }
+        public double getRawRightTriggerAxis()  { return getRawAxis(3); }
 
-        /*******************/
-        /*** LEFT BUMPER ***/
-        /*******************/
-        @Override
-        public boolean getRawLeftBumper() {
-            return getRawButton(5);
-        }
+        // Face Buttons //
+        public boolean getRawLeftButton()   { return getRawButton(1); }
+        public boolean getRawRightButton()  { return getRawButton(3); }
+        public boolean getRawTopButton()    { return getRawButton(4); }
+        public boolean getRawBottomButton() { return getRawButton(2); }
 
-        /********************/
-        /*** RIGHT BUMPER ***/
-        /********************/
-        @Override
-        public boolean getRawRightBumper() {
-            return getRawButton(6);
-        }
+        // Start / Select / Option //
+        public boolean getRawSelectButton() { return getRawButton(7); }
+        public boolean getRawStartButton()  { return getRawButton(8); }
+        public boolean getRawOptionButton() { return getRawButton(8); }
 
-        /********************/
-        /*** LEFT TRIGGER ***/
-        /********************/
-        @Override
-        public double getRawLeftTriggerAxis() {
-            return getRawAxis(2);
-        }
+        // Analog Stick Buttons // 
+        public boolean getRawLeftAnalogButton()     { return getRawButton(9); }
+        public boolean getRawRightAnalogButton()    { return getRawButton(10); }
 
-        /*********************/
-        /*** RIGHT TRIGGER ***/
-        /*********************/
-        @Override
-        public double getRawRightTriggerAxis() {
-            return getRawAxis(3);
-        }
-
-        /*******************/
-        /*** LEFT BUTTON ***/
-        /*******************/
-        @Override
-        public boolean getRawLeftButton() {
-            return getRawButton(3);
-        }
-
-        /********************/
-        /*** RIGHT BUTTON ***/
-        /********************/
-        @Override
-        public boolean getRawRightButton() {
-            return getRawButton(2);
-        }
-
-        /******************/
-        /*** TOP BUTTON ***/
-        /******************/
-        @Override
-        public boolean getRawTopButton() {
-            return getRawButton(4);
-        }
-
-        /*********************/
-        /*** BOTTOM BUTTON ***/
-        /*********************/
-        @Override
-        public boolean getRawBottomButton() {
-            return getRawButton(1);
-        }
-
-        /*********************/
-        /*** SELECT BUTTON ***/
-        /*********************/
-        @Override
-        public boolean getRawSelectButton() {
-            return getRawButton(7);
-        }
-
-        /********************/
-        /*** START BUTTON ***/
-        /********************/
-        @Override
-        public boolean getRawStartButton() {
-            return getRawButton(8);
-        }
-
-        /**************************/
-        /*** LEFT ANALOG BUTTON ***/
-        /**************************/
-        @Override
-        public boolean getRawLeftAnalogButton() {
-            return getRawButton(9);
-        }
-
-        /***************************/
-        /*** RIGHT ANALOG BUTTON ***/
-        /***************************/
-        @Override
-        public boolean getRawRightAnalogButton() {
-            return getRawButton(10);
-        }
-
-        /**************/
-        /*** RUMBLE ***/
-        /**************/
-        @Override
+        // Rumble //
         public void setRumble(double intensity) {
             getJoystick().setRumble(GenericHID.RumbleType.kLeftRumble, intensity);
             getJoystick().setRumble(GenericHID.RumbleType.kRightRumble, intensity);
--- src/com/stuypulse/stuylib/input/gamepads/PS4.java
+++ src/com/stuypulse/stuylib/input/gamepads/PS4.java
@@ -12,168 +12,49 @@ import edu.wpi.first.wpilibj.GenericHID;
 
 public class PS4 extends WPIGamepad {
 
-    /********************/
-    /*** CONSTRUCTORS ***/
-    /********************/
-
+    // Constructor //
     public PS4(int port) {
         super(port);
     }
 
-    /**********************/
-    /*** CONTROL STICKS ***/
-    /**********************/
-    @Override
-    public double getLeftX() {
-        return getRawAxis(0);
-    }
+    // Left Stick //
+    public double getLeftX()    { return getRawAxis(0); }
+    public double getLeftY()    { return -getRawAxis(1); }
 
-    @Override
-    public double getLeftY() {
-        return -getRawAxis(1);
-    }
+    // Right Stick //
+    public double getRightX()   { return getRawAxis(2); }
+    public double getRightY()   { return -getRawAxis(5); }
 
-    @Override
-    public double getRightX() {
-        return getRawAxis(2);
-    }
+    // D-Pad //
+    public boolean getRawDPadUp()       { return getJoystick().getPOV() == 0; }
+    public boolean getRawDPadDown()     { return getJoystick().getPOV() == 180; }
+    public boolean getRawDPadLeft()     { return getJoystick().getPOV() == 270; }
+    public boolean getRawDPadRight()    { return getJoystick().getPOV() == 90; }
 
-    @Override
-    public double getRightY() {
-        return -getRawAxis(5);
-    }
+    // Bumpers //
+    public boolean getRawLeftBumper()   { return getRawButton(5); }
+    public boolean getRawRightBumper()  { return getRawButton(6); }
+    
+    // Triggers //
+    public double getRawLeftTriggerAxis()   { return (getRawAxis(3) + 1.0) / 2.0; }
+    public double getRawRightTriggerAxis()  { return (getRawAxis(4) + 1.0) / 2.0; }
 
-    /**********************/
-    /*** D-PAD CONTROLS ***/
-    /**********************/
-    @Override
-    public boolean getRawDPadUp() {
-        return getJoystick().getPOV() == 0;
-    }
+    // Face Buttons //
+    public boolean getRawLeftButton()   { return getRawButton(1); }
+    public boolean getRawRightButton()  { return getRawButton(3); }
+    public boolean getRawTopButton()    { return getRawButton(4); }
+    public boolean getRawBottomButton() { return getRawButton(2); }
 
-    @Override
-    public boolean getRawDPadDown() {
-        return getJoystick().getPOV() == 180;
-    }
+    // Start / Select / Option //
+    public boolean getRawSelectButton() { return getRawButton(9); }
+    public boolean getRawStartButton()  { return getRawButton(10); }
+    public boolean getRawOptionButton() { return getRawButton(10); }
+    
+    // Analog Stick Buttons // 
+    public boolean getRawLeftAnalogButton()     { return getRawButton(11); }
+    public boolean getRawRightAnalogButton()    { return getRawButton(12); }
 
-    @Override
-    public boolean getRawDPadLeft() {
-        return getJoystick().getPOV() == 270;
-    }
-
-    @Override
-    public boolean getRawDPadRight() {
-        return getJoystick().getPOV() == 90;
-    }
-
-    /*******************/
-    /*** LEFT BUMPER ***/
-    /*******************/
-    @Override
-    public boolean getRawLeftBumper() {
-        return getRawButton(5);
-    }
-
-    /********************/
-    /*** RIGHT BUMPER ***/
-    /********************/
-    @Override
-    public boolean getRawRightBumper() {
-        return getRawButton(6);
-    }
-
-    /********************/
-    /*** LEFT TRIGGER ***/
-    /********************/
-    @Override
-    public double getRawLeftTriggerAxis() {
-        return (getRawAxis(3) + 1.0) / 2.0;
-    }
-
-    /*********************/
-    /*** RIGHT TRIGGER ***/
-    /*********************/
-    @Override
-    public double getRawRightTriggerAxis() {
-        return (getRawAxis(4) + 1.0) / 2.0;
-    }
-
-    /*******************/
-    /*** LEFT BUTTON ***/
-    /*******************/
-    @Override
-    public boolean getRawLeftButton() {
-        return getRawButton(1);
-    }
-
-    /********************/
-    /*** RIGHT BUTTON ***/
-    /********************/
-    @Override
-    public boolean getRawRightButton() {
-        return getRawButton(3);
-    }
-
-    /******************/
-    /*** TOP BUTTON ***/
-    /******************/
-    @Override
-    public boolean getRawTopButton() {
-        return getRawButton(4);
-    }
-
-    /*********************/
-    /*** BOTTOM BUTTON ***/
-    /*********************/
-    @Override
-    public boolean getRawBottomButton() {
-        return getRawButton(2);
-    }
-
-    /*********************/
-    /*** SELECT BUTTON ***/
-    /*********************/
-    @Override
-    public boolean getRawSelectButton() {
-        return getRawButton(9);
-    }
-
-    /********************/
-    /*** START BUTTON ***/
-    /********************/
-    @Override
-    public boolean getRawStartButton() {
-        return getRawButton(10);
-    }
-
-    /**************************/
-    /*** LEFT ANALOG BUTTON ***/
-    /**************************/
-    @Override
-    public boolean getRawLeftAnalogButton() {
-        return getRawButton(11);
-    }
-
-    /***************************/
-    /*** RIGHT ANALOG BUTTON ***/
-    /***************************/
-    @Override
-    public boolean getRawRightAnalogButton() {
-        return getRawButton(12);
-    }
-
-    /*********************/
-    /*** OPTION BUTTON ***/
-    /*********************/
-    @Override
-    public boolean getRawOptionButton() {
-        return getRawButton(10);
-    }
-
-    /**************/
-    /*** RUMBLE ***/
-    /**************/
-    @Override
+    // Rumble //
     public void setRumble(double intensity) {
         getJoystick().setRumble(GenericHID.RumbleType.kLeftRumble, intensity);
         getJoystick().setRumble(GenericHID.RumbleType.kRightRumble, intensity);
