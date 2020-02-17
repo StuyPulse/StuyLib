1e7fa91c4d0703bf853fe2896baf56a196ab640e
Sam Belliveau <sam.belliveau@gmail.com>
2020-02-16 19:00:58 -0500

    made weighted moving average WAY faster

--- src/com/stuypulse/stuylib/streams/filters/WeightedMovingAverage.java
+++ src/com/stuypulse/stuylib/streams/filters/WeightedMovingAverage.java
@@ -3,9 +3,6 @@ package com.stuypulse.stuylib.streams.filters;
 import com.stuypulse.stuylib.streams.filters.IStreamFilter;
 import com.stuypulse.stuylib.exception.ConstructionError;
 
-import java.util.Queue;
-import java.util.LinkedList;
-
 /**
  * Implementation of Weighted Moving Average.
  * 
@@ -16,44 +13,22 @@ import java.util.LinkedList;
 
 public class WeightedMovingAverage implements IStreamFilter {
 
-    private int mSize;
     private double mTotal;
-    private Queue<Double> mValues;
+    private MovingAverage mValues;
 
     /**
-     * Make Simple Moving Average with Max Array Size
-     * 
-     * @param size size of moving average
+     * @param size size of weighted moving average
      */
     public WeightedMovingAverage(int size) throws ConstructionError {
         if (size <= 0) {
             throw new ConstructionError("MovingAverage(int size)", "size must be greater than 0!");
         }
 
-        mSize = size;
-        mValues = new LinkedList<>();
+        mValues = new MovingAverage(size);
         mTotal = 0.0;
-
-        while (mValues.size() < mSize) {
-            mValues.add(0.0);
-        }
     }
 
     public double get(double next) {
-
-        // Remove sum from
-        for(double a : mValues) { 
-            mTotal -= a;
-        }
-
-        // Remove old value
-        mValues.remove();
-
-        // Add new value
-        mValues.add(next);
-        mTotal += next * mSize;
-
-        // Return average
-        return mTotal / ((mSize * mSize + mSize) * 0.5);
+        return mTotal += next - mValues.get(next);
     }
 }
