50804f95b562060b1581f6f54b402463759887cc
myles p <Myles Pasetsky>
2020-02-17 16:24:35 -0500

    Add x and y data to chart

--- src/com/stuypulse/stuylib/chart/Chart.java
+++ src/com/stuypulse/stuylib/chart/Chart.java
@@ -1,20 +1,47 @@
 package com.stuypulse.stuylib.chart;
 
-import com.stuypulse.stuylib.exception.ConstructionError;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.XYChart;
 
 public class Chart {
 
+    /**
+     * TODO:
+     * add functionality to reset adn replace xData and yData with custom lists
+     * reset xData and yData
+     * creat NChart with N-number of series
+     */
+
     private XYChart instance;
+    private List<Double> xData;
+    private List<Double> yData;
 
     protected XYChart get() {
         return instance;
     }
 
+    protected void update(double x, double y) {
+        xData.add(x);
+        yData.add(y);
+    }
+
+    protected List<Double> getXData() {
+        return xData;
+    }
+
+    protected List<Double> getYData() {
+        return yData;
+    }
+
     public Chart(String title, String x, String y) {
-        instance = QuickChart.getChart(title,x,y,title, new double[] {0.0},new double[] {0.0});
+        xData = new ArrayList<Double>();
+        yData = new ArrayList<Double>();
+        xData.add(0.0);
+        yData.add(0.0);
+        instance = QuickChart.getChart(title,x,y, title, xData, yData);
     }
 
 }
