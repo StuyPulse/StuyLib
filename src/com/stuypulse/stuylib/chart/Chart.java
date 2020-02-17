117de220a33833c8119c62c183bad715b9d64577
myles p <Myles Pasetsky>
2020-02-17 12:38:15 -0500

    Remove button bindings.

--- src/com/stuypulse/stuylib/chart/Chart.java
+++ src/com/stuypulse/stuylib/chart/Chart.java
@@ -7,27 +7,14 @@ import org.knowm.xchart.XYChart;
 
 public class Chart {
 
-    private final int binding;
     private XYChart instance;
 
-    public XYChart get() {
+    protected XYChart get() {
         return instance;
     }
 
-    public int getBinding() {
-        return binding;
-    }
-
-    public Chart(String title, String x, String y, int e) {
-        if (e < 0) {
-            throw new ConstructionError("Chart(String title, String x, String y, int e)","Binding must not be less than 0");
-        }
-        instance = QuickChart.getChart(title,x,y,title, new double[] {0.0},new double[] {0.0});
-        binding = e;
-    }
-
     public Chart(String title, String x, String y) {
-        this(title,x,y,-1);
+        instance = QuickChart.getChart(title,x,y,title, new double[] {0.0},new double[] {0.0});
     }
 
 }
