4ceb0a836f7d46be8cff3f20e0895379927bf1be
myles p <Myles Pasetsky>
2020-02-19 14:53:11 -0500

    Use Set<> for getting bools. Add lambda keybinds.

--- src/com/stuypulse/stuylib/util/chart/KeyTracker.java
+++ src/com/stuypulse/stuylib/util/chart/KeyTracker.java
@@ -2,82 +2,57 @@ package com.stuypulse.stuylib.util.chart;
 
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 /**
- * @author Myles Pasetsky (selym3) 
+ * @author Myles Pasetsky (selym3)
  */
 public class KeyTracker extends KeyAdapter {
 
-    private final ChartGroup area;
-
-    private List<Integer> keys;
-    private Map<Integer, Boolean> values;
-
-    public KeyTracker(ChartGroup area) {
-        this.area = area;
-
-        keys = new ArrayList<Integer>();
-        values = new HashMap<Integer, Boolean>();
+    public interface BindingFunction {
+        void function();
     }
 
-    public KeyTracker(ChartGroup area, int... e) {
-        this(area);
-        addKey(e);
+    private Set<Integer> values;
+    private Map<Integer, BindingFunction> bindings;
+
+    public KeyTracker() {
+        values = new HashSet<Integer>();
+        bindings = new HashMap<Integer, BindingFunction>();
     }
 
-    /**
-     * Sets the values to v if key is d
-     * 
-     * @param d Target key
-     * @param v Target boolean
-     */
-    private void setValues(int d, boolean v) {
-        for (Integer keyBinding : values.keySet()) {
-            if (keyBinding == d) {
-                values.replace(keyBinding, v);
-            }
-        }
+    public boolean getKey(int binding) {
+        return values.contains(binding);
+    }
+
+    public KeyTracker addBinding(int keyCode, BindingFunction bindingFunction) {
+        bindings.put(keyCode, bindingFunction);
+        return this;
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        // NECESSARY FUNCTIONS FOR A CHARTGROUP
         int keyCode = e.getKeyCode();
         if (keyCode == KeyEvent.VK_ESCAPE) {
             System.exit(1);
         }
 
-        if (keyCode == KeyEvent.VK_RIGHT) {
-            area.getCardLayout().next(area.getContentPane());
-        } else if (keyCode == KeyEvent.VK_LEFT) {
-            area.getCardLayout().previous(area.getContentPane());
+        for (Map.Entry<Integer, BindingFunction> entry : bindings.entrySet()) {
+            if (entry.getKey() == keyCode) {
+                entry.getValue().function();
+            }
         }
 
-        setValues(keyCode, true);
+        values.add(keyCode);
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         int keyCode = e.getKeyCode();
-        setValues(keyCode, false);
-    }
-
-    public boolean getKey(int binding) {
-        return values.get(binding);
-    }
-
-    public KeyTracker addKey(int... e) {
-        for (int binding : e) {
-            keys.add(binding);
-        }
-        for (Integer key : keys) {
-            values.put(key,false);
-        }
-        return this;
+        values.remove(keyCode);
     }
 
 }
