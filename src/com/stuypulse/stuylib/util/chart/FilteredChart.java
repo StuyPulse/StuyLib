2f4ec01f2b9c172444d8f9284b9c85d8e34cfe19
myles p <Myles Pasetsky>
2020-02-19 14:51:44 -0500

    Chart with a filter that overrides update(y).

--- src/com/stuypulse/stuylib/util/chart/FilteredChart.java
+++ src/com/stuypulse/stuylib/util/chart/FilteredChart.java
@@ -1,6 +1,8 @@
 package com.stuypulse.stuylib.util.chart;
 
+import com.stuypulse.stuylib.math.SLMath;
 import com.stuypulse.stuylib.streams.filters.IStreamFilter;
+import com.stuypulse.stuylib.streams.filters.RollingAverage;
 
 /**
  * A single session chart with a stream filter.
@@ -9,6 +11,8 @@ import com.stuypulse.stuylib.streams.filters.IStreamFilter;
  */
 public class FilteredChart extends Chart {
 
+    private static final long serialVersionUID = 1L;
+    
     private final IStreamFilter filter;
 
     /**
@@ -21,6 +25,10 @@ public class FilteredChart extends Chart {
         this.filter = filter;
     }
 
+    public FilteredChart(String title, IStreamFilter filter) { 
+        this(title,"x","y",filter);
+    }
+
     /**
      * Update function used by FilteredChart to apply the filter. Must be used
      * because it autoincrements the x-value, as to not mess with time-dependent
@@ -30,7 +38,22 @@ public class FilteredChart extends Chart {
      */
     @Override
     public void update(double y) {
-        update(xData.get(xData.size() - 1) + 1,filter.get(y));
+        update(getXData().get(getXData().size() - 1) + 1,filter.get(y));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Chart chart = new FilteredChart("Title", new RollingAverage(24)).setMaxSize(694).setYBounds(0.0, 1.0).display();
+
+        double y = 0;
+        while (true) {
+            y = SLMath.limit((chart.getMouseY() - 0.05) / 0.9, 0, 1);
+
+            chart.update(y);
+
+            chart.repaint();
+
+            Thread.sleep(50);
+        }
     }
 
 }
