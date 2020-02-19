eda07d45220a335835dd1c22906b2d94a926547a
myles p <Myles Pasetsky>
2020-02-18 19:06:30 -0500

    Add ability to map key presses to their key code.

--- src/com/stuypulse/stuylib/util/chart/KeyTracker.java
+++ src/com/stuypulse/stuylib/util/chart/KeyTracker.java
@@ -1,20 +1,51 @@
 package com.stuypulse.stuylib.util.chart;
 
-import java.awt.event.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Myles Pasetsky (selym3) 
  */
 public class KeyTracker extends KeyAdapter {
 
-    private final QuickWrapper area;
+    private final ChartGroup area;
 
-    public KeyTracker(QuickWrapper area) {
+    private List<Integer> keys;
+    private Map<Integer, Boolean> values;
+
+    public KeyTracker(ChartGroup area) {
         this.area = area;
+
+        keys = new ArrayList<Integer>();
+        values = new HashMap<Integer, Boolean>();
+    }
+
+    public KeyTracker(ChartGroup area, int... e) {
+        this(area);
+        addKey(e);
+    }
+
+    /**
+     * Sets the values to v if key is d
+     * 
+     * @param d Target key
+     * @param v Target boolean
+     */
+    private void setValues(int d, boolean v) {
+        for (Integer keyBinding : values.keySet()) {
+            if (keyBinding == d) {
+                values.replace(keyBinding, v);
+            }
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
+        // NECESSARY FUNCTIONS FOR A CHARTGROUP
         int keyCode = e.getKeyCode();
         if (keyCode == KeyEvent.VK_ESCAPE) {
             System.exit(1);
@@ -25,5 +56,28 @@ public class KeyTracker extends KeyAdapter {
         } else if (keyCode == KeyEvent.VK_LEFT) {
             area.getCardLayout().previous(area.getContentPane());
         }
+
+        setValues(keyCode, true);
     }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        int keyCode = e.getKeyCode();
+        setValues(keyCode, false);
+    }
+
+    public boolean getKey(int binding) {
+        return values.get(binding);
+    }
+
+    public KeyTracker addKey(int... e) {
+        for (int binding : e) {
+            keys.add(binding);
+        }
+        for (Integer key : keys) {
+            values.put(key,false);
+        }
+        return this;
+    }
+
 }
