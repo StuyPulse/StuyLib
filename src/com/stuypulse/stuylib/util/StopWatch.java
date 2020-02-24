52ac22a9662b77fb91e953b1a4ca003dced8e832
Sam Belliveau <sam.belliveau@gmail.com>
2020-02-24 11:36:33 -0500

    added LinearFilter

--- src/com/stuypulse/stuylib/util/StopWatch.java
+++ src/com/stuypulse/stuylib/util/StopWatch.java
@@ -11,28 +11,31 @@ package com.stuypulse.stuylib.util;
  */
 public class StopWatch {
 
-    // These are the functions that provide the time data. If you wish to change
-    // the
-    // way that the StopWatch class gets its time data, change that here.
-    private static double kTimerScale = 1_000_000_000;
+    // Engine interface used to get the current time
+    public interface TimeEngine {
+        // Get the raw time as a long
+        public long getRawTime();
 
-    private static long getCurrentTime() {
-        return System.nanoTime();
+        // Convert the integer time into a double
+        public double toSeconds(long raw);
     }
 
-    private static double toSeconds(long time) {
-        return Math.max(time, 1) / kTimerScale;
-    }
-
-    // The start time of the stopwatch
-    // This is the return type of System.nanoTime.
+    private TimeEngine mEngine;
     private long mLastTime;
 
     /**
      * Creates timer and reset it to now.
      */
+    public StopWatch(TimeEngine engine) {
+        mEngine = engine;
+        mLastTime = mEngine.getRawTime();
+    }
+
+    /**
+     * Creates timer and reset it to now.
+     */
     public StopWatch() {
-        mLastTime = StopWatch.getCurrentTime();
+        this(kNanoEngine);
     }
 
     /**
@@ -43,10 +46,10 @@ public class StopWatch {
      *         is always a non 0 positive number.
      */
     public double reset() {
-        long time = StopWatch.getCurrentTime();
-        long delta = time - mLastTime;
+        long time = mEngine.getRawTime();
+        long delta = Math.max(1, time - mLastTime);
         mLastTime = time;
-        return StopWatch.toSeconds(delta);
+        return mEngine.toSeconds(delta);
     }
 
     /**
@@ -56,7 +59,35 @@ public class StopWatch {
      *         is always a non 0 positive number.
      */
     public double getTime() {
-        long delta = StopWatch.getCurrentTime() - mLastTime;
-        return StopWatch.toSeconds(delta);
+        long delta = Math.max(1, mEngine.getRawTime() - mLastTime);
+        return mEngine.toSeconds(delta);
     }
+
+    /**
+     * This engine is used to get the current time with the system function System.nanoTime()
+     */
+    public static final TimeEngine kNanoEngine = new TimeEngine() {
+        public long getRawTime() {
+            return System.nanoTime();
+        }
+
+        public double toSeconds(long raw) {
+            return raw / 1_000_000_000.0;
+        }
+    };
+
+    /**
+     * This engine is used to get the current time with the system function System.currentTimeMillis()
+     * 
+     * This may have a lower resolution, but it is stable. It is no
+     */
+    public static final TimeEngine kMillisEngine = new TimeEngine() {
+        public long getRawTime() {
+            return System.currentTimeMillis();
+        }
+
+        public double toSeconds(long raw) {
+            return raw / 1_000.0;
+        }
+    };
 }
