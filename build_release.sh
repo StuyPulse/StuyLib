f60487c26c23d46e16fc1fa718ddea66d27c89ed
Sam Belliveau <sam.belliveau@gmail.com>
2021-02-20 14:12:55 -0500

    fixed issue on windows

--- build_release.sh
+++ build_release.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 BUILD_FILE="./build/libs/StuyLib-1.0.0.jar"
+OUTPUT_FILE="./releases/StuyLib-$1.jar"
 
 if [ -z "$1" ]
   then
@@ -8,14 +9,15 @@ if [ -z "$1" ]
     echo "usage: $0 [version]"
 else 
     echo "Removing Old Builds..."
-    rm -rf "./build/libs"
+    rm -f $BUILD_FILE
+    rm -f $OUTPUT_FILE
 
     echo "Force Building Project..."
     ./force_build.sh
 
     if test -f $BUILD_FILE; then 
         mkdir ./releases >/dev/null 2>&1
-        cp $BUILD_FILE "./releases/StuyLib-$1.jar" >/dev/null 2>&1
+        cp $BUILD_FILE $OUTPUT_FILE >/dev/null 2>&1
         echo ""
         echo "Succesfully Built StuyLib-$1.jar to ./releases/"
     else
