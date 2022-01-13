6ccadc1499e58e6a1acb41b621ac7a5989a8a1ee
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-01-13 13:38:05 -0500

    Implement constructors

--- src/com/stuypulse/stuylib/math/Matrix.java
+++ src/com/stuypulse/stuylib/math/Matrix.java
@@ -5,10 +5,55 @@
 package com.stuypulse.stuylib.math;
 
 /**
- * Multiplication-supporting matrix class.
+ * Multiplication-supporting matrix class containing doubles.
  *
  * @author Ivan Wei (ivanw8288@gmail.com)
  */
 public final class Matrix {
+    private double[][] data;
+
+    /**
+     * Constructs a square matrix with the given size, where all values are initialized to 0.0.
+     * 
+     * @param size the size of the square matrix
+     */
+    public Matrix(int size) {
+        this.data = new double[size][size];
+    }
+
+    /**
+     * Constructs a matrix with the given number of rows and columns, where all values are initialized to 0.0.
+     * 
+     * @param rows the number of rows in the matrix
+     * @param cols the number of cols in the matrix
+     */
+    public Matrix(int rows, int cols) {
+        this.data = new double[rows][cols];
+    }
     
+    /**
+     * Constructs a matrix with the given array of doubles. Throws an exception if the rows are not uniform in length. 
+     * 
+     * @param data 
+     */
+    public Matrix(double[][] data) {
+        if (data.length <= 0) {
+            this.data = new double[0][0];
+            return;
+        }
+
+        this.data = new double[data.length][data[0].length];
+
+        int columns = data[0].length;
+        for (int r = 0; r < data.length; ++r) {
+            if (data[r].length != columns) {
+                throw new IllegalArgumentException("Could not construct matrix; given matrix data is not of uniform length");
+            }
+            
+            for (int c = 0; c < data[r].length; ++c) {
+                this.data[r][c] = data[r][c];
+            }
+        }
+    }
+
 }
