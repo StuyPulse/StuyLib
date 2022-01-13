f6e6650d678cf7ebc30f0235815f96c9d998bb96
iwei20 <44007607+iwei20@users.noreply.github.com>
2022-01-13 12:20:36 -0500

    Create Matrix file

---
+++ src/com/stuypulse/stuylib/math/Matrix.java
@@ -0,0 +1,14 @@
+/* Copyright (c) 2021 StuyPulse Robotics. All rights reserved. */
+/* This work is licensed under the terms of the MIT license */
+/* found in the root directory of this project. */
+
+package com.stuypulse.stuylib.math;
+
+/**
+ * Multiplication-supporting matrix class.
+ *
+ * @author Ivan Wei (ivanw8288@gmail.com)
+ */
+public final class Matrix {
+    
+}
