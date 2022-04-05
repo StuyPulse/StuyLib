ca565f4fb0407f7257f6d6b44c91f8fdce5ae535
Sam Belliveau <sam.belliveau@gmail.com>
2022-04-04 21:14:35 -0400

    Improved Low Pass Filter

--- src/com/stuypulse/stuylib/streams/filters/HighPassFilter.java
+++ src/com/stuypulse/stuylib/streams/filters/HighPassFilter.java
@@ -4,8 +4,6 @@
 
 package com.stuypulse.stuylib.streams.filters;
 
-import com.stuypulse.stuylib.util.StopWatch;
-
 /**
  * Implementation for of a real time IIR HighPassFilter
  *
@@ -13,29 +11,14 @@ import com.stuypulse.stuylib.util.StopWatch;
  */
 public class HighPassFilter implements IFilter {
 
-    private StopWatch mTimer;
-
-    private double mLastValue;
-    private double mLastInput;
-    private Number mRC;
+    private LowPassFilter mInverse;
 
     /** @param rc time constant for high pass filter */
     public HighPassFilter(Number rc) {
-        if (rc.doubleValue() < 0) {
-            throw new IllegalArgumentException("rc must be a positive number");
-        }
-
-        mTimer = new StopWatch();
-        mLastValue = 0;
-        mLastInput = 0;
-        mRC = rc;
+        mInverse = new LowPassFilter(rc);
     }
 
     public double get(double next) {
-        double dt = mTimer.reset();
-        double a = mRC.doubleValue() / (mRC.doubleValue() + dt);
-        mLastValue = a * (mLastValue + next - mLastInput);
-        mLastInput = next;
-        return mLastValue;
+        return next - mInverse.get(next);
     }
 }
