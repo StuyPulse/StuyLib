ec4acb7c74b1fb23d181e42d67474e77e9de78b5
Sam Belliveau <sam.belliveau@gmail.com>
2022-06-01 17:38:10 -0400

    fixed dumb comment

--- src/com/stuypulse/stuylib/streams/filters/MotorProfile.java
+++ src/com/stuypulse/stuylib/streams/filters/MotorProfile.java
@@ -8,11 +8,10 @@ import com.stuypulse.stuylib.math.SLMath;
 import com.stuypulse.stuylib.util.StopWatch;
 
 /**
- * A filter, that when applied to the input of a motor, will characterize it. Similar to the way in
- * which motion profiling can limit the amount of acceleration and jerk in an S-Curve, this can do
- * that to real time input. Because this will add a delay, it is recommended that the jerklimit is
- * as high as possible. Aside from the jerklimit, this is identicle to a SlewRateLimiter or
- * TimedRateLimit.
+ * A filter, that when applied to the input of a motor, will profile it. Similar to the way in which
+ * motion profiling can limit the amount of acceleration and jerk in an S-Curve, this can do that to
+ * real time input. Because this will add a delay, it is recommended that the jerklimit is as high
+ * as possible. Aside from the jerklimit, this is identicle to a SlewRateLimiter or TimedRateLimit.
  *
  * @author Sam (sam.belliveau@gmail.com)
  */
