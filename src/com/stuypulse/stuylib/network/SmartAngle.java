28d64d57f8f775985d24b7a374fc68e307c5d1c5
Myles Pasetsky <mp3blue99@gmail.com>
2022-06-18 10:18:54 -0400

    Add documentation, clean SmartAngle API

--- src/com/stuypulse/stuylib/network/SmartAngle.java
+++ src/com/stuypulse/stuylib/network/SmartAngle.java
@@ -1,72 +1,148 @@
 package com.stuypulse.stuylib.network;
 
-import java.util.function.Function;
+import java.util.function.Supplier;
 
 import com.stuypulse.stuylib.math.Angle;
 import com.stuypulse.stuylib.util.Conversion;
 
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.networktables.EntryListenerFlags;
 import edu.wpi.first.networktables.EntryNotification;
 import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class SmartAngle {
+/**
+ * SmartAngle works as a wrapper for numbers on the network. This class
+ * handles converting a double on the network to an Angle 
+ * 
+ * @author Myles Pasetsky (myles.pasetsky@gmail.com)
+ */
+public class SmartAngle implements Supplier<Angle> {
     
+    // Flags for when the network table entry triggers an update
     private static final int LISTENER_FLAGS = EntryListenerFlags.kUpdate | EntryListenerFlags.kNew;
     
-    private static final Conversion<Double, Angle> DEGREES = Conversion.make(Angle::fromDegrees, a->a.toDegrees());
-    private static final Conversion<Double, Angle> RADIANS = Conversion.make(Angle::fromRadians, a->a.toRadians());
+    // Built-in conversions from network doubles to Angles
+    public static final Conversion<Double, Angle> kDegrees = Conversion.make(Angle::fromDegrees, a->a.toDegrees());
+    public static final Conversion<Double, Angle> kRadians = Conversion.make(Angle::fromRadians, a->a.toRadians());
 
+    // network table entry and its default value
     private NetworkTableEntry mEntry;
-    private double mDefaultValue;
+    private Angle mDefaultValue;
 
+    // the double<->angle conversion being used by this smart angle 
     private Conversion<Double, Angle> mConversion;
 
+    // the angle from the network value, updated only when network value changes
     private Angle mAngle;
 
-    public SmartAngle(NetworkTableEntry entry, double value) {
-        degrees();
-
+    /**
+     * Create a SmartAngle with a NetworkTableEntry and a default
+     * angle value
+     * 
+     * @param entry entry to wrap
+     * @param value default angle value
+     */
+    public SmartAngle(NetworkTableEntry entry, Angle value) {
         mEntry = entry;
         mDefaultValue = value;
-        mEntry.setDefaultValue(value);
+        mEntry.setDefaultDouble(mConversion.from(mDefaultValue));
         
         mEntry.addListener(this::update, LISTENER_FLAGS);
+        degrees();
     }
 
-    public SmartAngle(NetworkTableEntry entry, Angle value) {
-        this(entry, )
+    /**
+     * Create a SmartAngle with a network entry key and a 
+     * default angle value
+     * 
+     * @param id network entry key
+     * @param value default value
+     */
+    public SmartAngle(String id, Angle value) {
+        this(SmartDashboard.getEntry(id), value);
     }
 
-    private void update(double value) {
+    /**
+     * Forcibly updates the stored angle given an entry update object
+     * 
+     * @param notif entry update notification
+     */
+    private void update(EntryNotification notif) {
+        double value = notif.value.getDouble();
         mAngle = mConversion.to(value);
     }
 
-    private void update(EntryNotification notif) {
-        update(notif.value.getDouble());
-    }
-
+    /**
+     * Sets the conversion from the double stored on the network to
+     * an Angle class (e.g. sets what unit the double on the network is in)
+     * 
+     * @param conversion conversion between Double and Angle
+     * @return reference to this
+     */
     public SmartAngle units(Conversion<Double, Angle> conversion) {
         mConversion = conversion;
         return this;
     }
 
+    /**
+     * Sets the unit of the network double to be radians
+     * 
+     * @return reference to this
+     */
     public SmartAngle radians() {
-        return units(RADIANS);
+        return units(kRadians);
     }
 
+    /**
+     * Sets the unit of the network double to be degrees
+     * 
+     * @return reference to this
+     */
     public SmartAngle degrees() {
-        return units(DEGREES);
+        return units(kDegrees);
     }
-    
+
+    /**
+     * @return the angle stored by the SmartAngle 
+     */
     public Angle getAngle() {
         return mAngle;
     }
 
-    public void set(Angle angle) {
-        set(mConversion.from(angle));
+    /**
+     * @return the angle stored by the SmartAngle as a Rotation2d
+     */
+    public Rotation2d getRotation2d() {
+        return getAngle().getRotation2d();
     }
 
-    public void set(double angle) {
-        mEntry.forceSetDouble(angle);
+    /**
+     * @return the angle stored by the SmartAngle
+     */
+    @Override
+    public Angle get() {
+        return getAngle();
+    }
+
+    /**
+     * sets the angle of SmartAngle.
+     */
+    public void set(Angle angle) {
+        mEntry.forceSetDouble(mConversion.from(angle));
+    }
+
+    /**
+     * resets the SmartAngle to its default value
+     */
+    public void reset() {
+        set(mDefaultValue);
+    }
+
+    /**
+     * @return the default value of the SmartAngle
+     */
+    public Angle getDefault() {
+        return mDefaultValue;
     }
 }
