0a2b2d45b0e601986ff0c6bebe99b4b1fadff593
Myles Pasetsky <mp3blue99@gmail.com>
2022-06-20 13:37:06 -0400

    finish XYStream

--- src/com/stuypulse/stuylib/util/plot/XYSeries.java
+++ src/com/stuypulse/stuylib/util/plot/XYSeries.java
@@ -3,31 +3,40 @@ package com.stuypulse.stuylib.util.plot;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.stuypulse.stuylib.math.Vector2D;
+import com.stuypulse.stuylib.streams.vectors.VStream;
+
 public class XYSeries extends Series {
 
     private List<Double> xValues;
     private List<Double> yValues;
 
-    public XYSeries(String name) {
+    private final VStream stream;
+
+    public XYSeries(String name, VStream stream) {
         super(name);
 
         xValues = new LinkedList<>();
         yValues = new LinkedList<>();
+
+        this.stream = stream;
     }
 
     @Override
     protected List<Double> getSafeXValues() {
-        return null;
+        return new LinkedList<>(xValues);
     }
 
     @Override
     protected List<Double> getSafeYValues() {
-        return null;
+        return new LinkedList<>(yValues);
     }
 
     @Override
     protected void update() {
-        
+        Vector2D next = stream.get();
+        xValues.add(next.x);
+        yValues.add(next.y);
     }
     
 }
