f3e8d41a2699c426ac186c902ebc42eb097f0fb2
Myles Pasetsky <mp3blue99@gmail.com>
2022-06-20 12:45:06 -0400

    Add mouse to playground

--- src/com/stuypulse/stuylib/util/plot/Playground.java
+++ src/com/stuypulse/stuylib/util/plot/Playground.java
@@ -1,7 +1,28 @@
 package com.stuypulse.stuylib.util.plot;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Playground {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Plot plot = new Plot(new Settings());
+
+        List<Double> x = new LinkedList<>();
+        List<Double> y = new LinkedList<>();
+
+        for (;;) {
+            x.add(plot.getMouseTracker().getMouseX());
+            y.add(plot.getMouseTracker().getMouseY());
+
+            while (x.size() > 250) x.remove(0);
+            while (y.size() > 250) y.remove(0);
+
+            plot.update("mouse", x, y);
+            plot.display();
+
+            Thread.sleep(20);
+        }
+
     }
 }
