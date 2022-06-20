b03ff00e3a8b00de7d10b2eab1c1815c5bc6b1c5
Myles Pasetsky <mp3blue99@gmail.com>
2022-06-20 15:24:07 -0400

    Add updated playground

--- src/com/stuypulse/stuylib/util/plot/Playground.java
+++ src/com/stuypulse/stuylib/util/plot/Playground.java
@@ -1,26 +1,40 @@
 package com.stuypulse.stuylib.util.plot;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import com.stuypulse.stuylib.streams.IStream;
+import com.stuypulse.stuylib.streams.vectors.VStream;
+import com.stuypulse.stuylib.streams.vectors.filters.VJerkLimit;
+import com.stuypulse.stuylib.util.plot.Series.Config;
 
 public class Playground {
-    public static void main(String[] args) throws InterruptedException {
-        Plot plot = new Plot(new Settings());
 
-        List<Double> x = new LinkedList<>();
-        List<Double> y = new LinkedList<>();
+    public interface Constants {
+        int DURATION = 100;
+
+        public static Series make(String id, IStream stream) {
+            return Series.make(new Config(id, DURATION), stream);
+        }
+
+        public static Series make(String id, VStream stream) {
+            return Series.make(new Config(id, DURATION), stream);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Plot plot = new Plot();
+        
+        VStream mouse = plot.getMouse()::getPosition;
+        IStream mouse_y = plot.getMouse()::getY;
+        
+        plot
+            // .addSeries(new TimeSeries("mouse_y", mouse_y))
+            // .addSeries(new XYSeries(new Config("mouse", 500), mouse))
+            .addSeries(Constants.make("y", mouse_y))
+            .addSeries(Constants.make("mouse", mouse))
+            .addSeries(Constants.make("jerk", mouse.filtered(new VJerkLimit(1.0, 10.0))))
+        ;
 
         for (;;) {
-            x.add(plot.getMouseTracker().getMouseX());
-            y.add(plot.getMouseTracker().getMouseY());
-
-            // while (x.size() > 250) x.remove(0);
-            // while (y.size() > 250) y.remove(0);
-
-            // plot.update("mouse", x, y);
-            // plot.display();
-
+            plot.update();
             Thread.sleep(20);
         }
 
