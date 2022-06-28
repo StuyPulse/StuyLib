beb7dd4963ce14cbea06184ee1edebefd26ceeee
Myles Pasetsky <mp3blue99@gmail.com>
2022-06-28 15:48:42 -0400

    Add playground with data graph

--- src/com/stuypulse/stuylib/util/plot/Playground.java
+++ src/com/stuypulse/stuylib/util/plot/Playground.java
@@ -8,30 +8,40 @@ import com.stuypulse.stuylib.math.Vector2D;
 import com.stuypulse.stuylib.math.interpolation.Interpolator;
 import com.stuypulse.stuylib.math.interpolation.NearestInterpolator;
 import com.stuypulse.stuylib.streams.IStream;
-import com.stuypulse.stuylib.streams.vectors.VStream;
 import com.stuypulse.stuylib.util.plot.Series.Config;
-
-import edu.wpi.first.math.util.Units;
+import com.stuypulse.stuylib.util.plot.TimeSeries.TimeSpan;
 
 public class Playground {
 
-    public interface Constants {
-        int DURATION = 100;
+    public static class Range implements IStream {
 
-        public static Series make(String id, IStream stream) {
-            return Series.make(new Config(id, DURATION), stream);
+        private double value;
+        private double delta;
+        private int steps;
+
+        public Range(double min, double max, int steps) {
+            this.value = min;
+            this.delta = (max-min)/steps;
+            this.steps = steps;
         }
 
-        public static Series make(String id, VStream stream) {
-            return Series.make(new Config(id, DURATION), stream);
+        public Range(double min, double max) {
+            this(min, max, (int)(max-min));
         }
+
+        @Override
+        public double get() {
+            if (steps-- < 0) return value;
+            return value += delta;
+        }
+
     }
 
     public interface Distances {
-        double RING = Units.inchesToMeters(150);
-        double POINT_A = Units.inchesToMeters(167);
-        double POINT_B = Units.inchesToMeters(184);
-        double LAUNCHPAD = Units.inchesToMeters(217);
+        double RING = 157;
+        double POINT_A = 167;
+        double POINT_B = 184;
+        double LAUNCHPAD = 217;
     }
 
     public interface RPMs {
@@ -41,81 +51,68 @@ public class Playground {
         double LAUNCHPAD = 3650;
     }
 
-    public interface Yaws {
-        double RING = 6.50694;
-        double POINT_A = 6.25;
-        double POINT_B = 5.70;
-        double LAUNCHPAD = 5;
-    }
-
-    public interface FirstShotMap {
-        Interpolator DIST_TO_RPM =
+    public interface Interpolators {
+        Interpolator TWO =
             new NearestInterpolator(
                 new Vector2D(Distances.RING, RPMs.RING),  
                 new Vector2D(Distances.LAUNCHPAD, RPMs.LAUNCHPAD));
 
-        Interpolator DIST_TO_YAW =
-            new NearestInterpolator(
-                new Vector2D(Distances.RING, Yaws.RING),
-                new Vector2D(Distances.LAUNCHPAD, Yaws.LAUNCHPAD));
-    }
-
-    public interface SecondShotMap {
-        Interpolator DIST_TO_RPM =
+        Interpolator FOUR =
             new NearestInterpolator(
                 new Vector2D(Distances.RING, RPMs.RING),
                 new Vector2D(Distances.POINT_A, RPMs.POINT_A),
                 new Vector2D(Distances.POINT_B, RPMs.POINT_B),    
                 new Vector2D(Distances.LAUNCHPAD, RPMs.LAUNCHPAD));
-
-        Interpolator DIST_TO_YAW =
-            new NearestInterpolator(
-                new Vector2D(Distances.RING, Yaws.RING),
-                new Vector2D(Distances.POINT_A, Yaws.POINT_A),
-                new Vector2D(Distances.POINT_B, Yaws.POINT_B),
-                new Vector2D(Distances.LAUNCHPAD, Yaws.LAUNCHPAD));
     }
 
-    public static void main(String[] args) throws InterruptedException {
-        double max_dist = 8;
+    public interface Constants {
+        int DURATION = 500;
 
-        Settings settings = new Settings();
+        String TITLE = "RPM vs Distance";
+        String X_AXIS = "Distance (inches)";
+        String Y_AXIS = "RPM";
 
-        settings.setXMax(8);
+        int WIDTH = 640;
+        int HEIGHT = 480;
 
-        // rpm range
-        settings.setYMin(1000);
-        settings.setYMax(4000);
+        double MIN_X = Distances.RING;
+        double MAX_X = Distances.LAUNCHPAD;
 
-        Plot plot = new Plot(settings);
+        double MIN_Y = RPMs.RING;
+        double MAX_Y = RPMs.LAUNCHPAD;
 
-        // mouse input on y axis is distance, output is time plot of rpm
-        // IStream mouse_y = IStream.create(plot.getMouse()::getY).filtered(x -> x * mouse_multiplier);
-        // plot.addSeries(Constants.make("ring shot", () -> RPMs.RING))
-        //         .addSeries(Constants.make("pad", () -> RPMs.LAUNCHPAD))
-        //         .addSeries(Constants.make("first rpm", mouse_y.filtered(FirstShotMap.DIST_TO_RPM)))
-        //         .addSeries(Constants.make("second rpm", mouse_y.filtered(SecondShotMap.DIST_TO_RPM)));
-                
-        // mouse input on x axis is distance, output is distance, rpm plot
-        IStream mouse_x = IStream.create(plot.getMouse()::getX).filtered(x -> x * max_dist);
-        plot.addSeries(Constants.make("ring shot", () -> new Vector2D(Distances.RING, RPMs.RING)))
-                .addSeries(Constants.make("pad", () -> new Vector2D(Distances.LAUNCHPAD, RPMs.LAUNCHPAD)))
-                .addSeries(Constants.make("first rpm", VStream.create(mouse_x, mouse_x.filtered(FirstShotMap.DIST_TO_RPM))))
-                .addSeries(Constants.make("second rpm", VStream.create(mouse_x, mouse_x.filtered(SecondShotMap.DIST_TO_RPM))));
-                
-        // mouse input on x axis is distance, outputs yaw, rpm plot
-        // plot.addSeries(Constants.make("ring shot", () -> new Vector2D(Yaws.RING, RPMs.RING)))
-        //         .addSeries(Constants.make("pad", () -> new Vector2D(Yaws.LAUNCHPAD, RPMs.LAUNCHPAD)))
-        //         .addSeries(Constants.make("first", VStream.create(
-        //             mouse_x.filtered(FirstShotMap.DIST_TO_YAW),
-        //             mouse_x.filtered(FirstShotMap.DIST_TO_RPM))))
-        //         .addSeries(Constants.make("second",VStream.create(
-        //             mouse_x.filtered(SecondShotMap.DIST_TO_YAW),
-        //             mouse_x.filtered(SecondShotMap.DIST_TO_RPM))));
+        Settings SETTINGS = new Settings()
+                .setSize(WIDTH, HEIGHT)
+                .setAxes(TITLE, X_AXIS, Y_AXIS)
+                .setXRange(MIN_X, MAX_X)
+                .setYRange(MIN_Y, MAX_Y)
+            ;
 
-        for (; ; ) {
-            plot.update();
-            Thread.sleep(20);
+        private static Range getRange() {
+            return new Range(MIN_X, MAX_X, DURATION);
         }
+
+        public static Series make(String id, Interpolator i) {
+            return new TimeSeries(
+                new Config(id, DURATION), 
+                new TimeSpan(MIN_X, MAX_X), 
+                getRange().filtered(i)
+            );
+        }
+    }
+
+
+    public static void main(String[] args) throws InterruptedException {
+        Plot plot = new Plot(Constants.SETTINGS);
+
+        plot.addSeries(Constants.make("2 points", Interpolators.TWO));
+        plot.addSeries(Constants.make("4 points", Interpolators.FOUR));
+
+
+        for (int i = 0; i < Constants.DURATION; ++i) {
+            plot.updateSeries();
+        }
+
+        plot.display();
     }
 }
