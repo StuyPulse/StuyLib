8a5139aa22ae61b65fb3b4dc64e26449e825fa7b
Myles Pasetsky <34213819+selym3@users.noreply.github.com>
2022-06-29 11:56:10 -0400

    Revert "Add an in place sort and lower bound finding to Interpolator (#47)" (#48)
    This reverts commit b8d173061312d7b3e269a198488ec881edc90301.

--- src/com/stuypulse/stuylib/math/interpolation/Interpolator.java
+++ src/com/stuypulse/stuylib/math/interpolation/Interpolator.java
@@ -12,9 +12,6 @@ import java.util.Arrays;
 /**
  * This class serves as a baseline for all other classes. The Interpolator is a filter that will
  * find the values of any point given a few reference points
- * 
- * @author Eric (ericlin071906@gmail.com)
- * @author Ivan Wei (ivanw8288@gmail.com)
  */
 public interface Interpolator extends IFilter {
 
@@ -23,6 +20,7 @@ public interface Interpolator extends IFilter {
      *
      * @param x point to be intepolated
      * @return interpolated value
+     * @author Eric Lin (ericlin071906@gmail.com)
      */
     double interpolate(double x);
 
@@ -31,43 +29,28 @@ public interface Interpolator extends IFilter {
     default double get(double x) {
         return interpolate(x);
     }
-    
-    /**
-     * Returns the index of the point with the greatest x-coordinate less than or equal to x, or -1 if it is lower than every element.
-     * Assumes that the array is sorted.
-     * 
-     * @param x the x-value to compare to
-     * @param points the points to find the lower bound among
-     * @return the index of the lower bound, or -1
-     */
-    public static int indexLowerBound(double x, Vector2D... points) {
-        for (int i = 0; i < points.length; ++i) {
-            if (points[i].x > x) {
-                return i - 1;
-            }
-        }
-        return points.length - 1;
-    }
 
     /**
-     * Performs an in-place sort by the value of x from smallest to greatest
-     * @param points the array for which to sort reference points
-     * @return the parameter array, for chaining
-     */
-    public static Vector2D[] sortPoints(Vector2D[] points) {
-        Arrays.sort(points, (lhs, rhs) -> (int) (Math.signum(lhs.x - rhs.x)));
-        return points;
-    }
-
-    /**
-     * Returns a sorted array of reference points by the value of x from smallest to greatest
+     * Sorts the reference point by the value of x from smallest to greatest
      *
      * @param points reference points
      * @return an array of sorted reference points
      */
     public static Vector2D[] getSortedPoints(Vector2D... points) {
+        if (points.length < 2) {
+            throw new IllegalArgumentException("Interpolation requires at least 2 points");
+        }
+
         Vector2D[] output = points.clone();
-        sortPoints(points);
+        Arrays.sort(points, (lhs, rhs) -> (int) (Math.signum(lhs.x - rhs.x)));
+
+        for (int i = 1; i < output.length; ++i) {
+            if (output[i - 1].x == output[i - 0].x) {
+                throw new IllegalArgumentException(
+                        "Interpolation requires all points to have unique x coordinates");
+            }
+        }
+
         return output;
     }
 }
