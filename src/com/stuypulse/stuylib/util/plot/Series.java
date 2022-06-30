ef616096d90965919312738be0bbed08435ac739
Myles Pasetsky <mp3blue99@gmail.com>
2022-06-29 22:17:41 -0400

    Make method protected

--- src/com/stuypulse/stuylib/util/plot/Series.java
+++ src/com/stuypulse/stuylib/util/plot/Series.java
@@ -64,7 +64,7 @@ public abstract class Series {
         }
     }
 
-    public final void update(XYChart chart) {
+    protected final void update(XYChart chart) {
         update();
 
         String name = getConfig().getName();
