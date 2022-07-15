9eb2ffb43434a373753258361625cd4433c2e1cd
Myles Pasetsky <mp3blue99@gmail.com>
2022-07-15 16:31:21 -0400

    revert redundant derivatives in ff controllers

--- src/com/stuypulse/stuylib/control/feedforward/PositionFeedforwardController.java
+++ src/com/stuypulse/stuylib/control/feedforward/PositionFeedforwardController.java
@@ -5,20 +5,17 @@ import com.stuypulse.stuylib.streams.filters.Derivative;
 public class PositionFeedforwardController extends FeedforwardController {
 
     private final Derivative mVelocity;
-    private final Derivative mAcceleration;
     private final Feedforward mFeedforward;
 
     public PositionFeedforwardController(Feedforward feedforward) {
         mVelocity = new Derivative();
-        mAcceleration = new Derivative();
         mFeedforward = feedforward;
     }
 
     @Override
     protected double calculate(double setpoint, double measurement) {
         double velocity = mVelocity.get(setpoint);
-        double acceleration = mAcceleration.get(velocity);
-        return mFeedforward.calculate(velocity, acceleration);
+        return mFeedforward.calculate(velocity);
     }
 
 
--- src/com/stuypulse/stuylib/control/feedforward/VelocityFeedforwardController.java
+++ src/com/stuypulse/stuylib/control/feedforward/VelocityFeedforwardController.java
@@ -1,22 +1,16 @@
 package com.stuypulse.stuylib.control.feedforward;
 
-import com.stuypulse.stuylib.streams.filters.Derivative;
-
 public class VelocityFeedforwardController extends FeedforwardController {
 
-    private final Derivative mAcceleration;
     private final Feedforward mFeedforward;
     
     public VelocityFeedforwardController(Feedforward feedforward) {
-        mAcceleration = new Derivative();
         mFeedforward = feedforward;
     }
 
     @Override
     protected double calculate(double setpoint, double measurement) {
-        double velocity = setpoint;
-        double acceleration = mAcceleration.get(velocity);
-        return mFeedforward.calculate(velocity, acceleration);
+        return mFeedforward.calculate(setpoint);
     }
     
 }
